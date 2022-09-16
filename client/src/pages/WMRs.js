6b5b4665011425a01bba8eb26528bddabdccf3b6
Quantumplatr <35316952+Quantumplatr@users.noreply.github.com>
2022-09-15 18:24:29 -0600

    9/16 WMR and Logs

--- client/src/pages/WMRs.js
+++ client/src/pages/WMRs.js
@@ -2,10 +2,16 @@ import React from 'react';
 import aug26 from "../files/WMRs/aug26.pdf";
 import sept2 from "../files/WMRs/sept2.pdf";
 import sept9 from "../files/WMRs/sept9_v2.pdf";
+import sept16 from "../files/WMRs/sept16.pdf";
 
 export default function WMRs() {
 
     var wmrList = [
+        {
+            "id": 4,
+            "title": "September 16, 2022",
+            "url": sept16
+        },
         {
             "id": 3,
             "title": "September 9, 2022",
